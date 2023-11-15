08f666e6795768f394ff756447e912f654eec0cd
ErikaWalker <erikapmagalhaes27@gmail.com>
2023-11-14 21:31:50 -0300

    ClinicaRomulus

---
+++ Models/Entities/Medico.cs
@@ -0,0 +1,16 @@
+public class Medico/*Inicio*/{
+
+    /*Variavel: É um espaço reservado na memoria do 
+    computador.*/
+
+    /*POO = Programação Orientada a Objeto*/
+
+    /*Na POO a variavel é chamada tambem de atributo*/
+
+    /*No C# ela pode ser uma propriedade automatica*/
+
+    int id;
+    string CRM = string.Empty;
+    string Nome = string.Empty; 
+
+}//Final
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClinicaRomulus.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
